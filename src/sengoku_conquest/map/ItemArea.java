095554756a4c10724bfa265417109665db25d15a
Crocus7724 <account@crocus7724.net>
2016-11-18 17:20:45 +0900

    [Add]getter setter追加

--- src/sengoku_conquest/map/ItemArea.java
+++ src/sengoku_conquest/map/ItemArea.java
@@ -6,8 +6,12 @@ import sengoku_conquest.item.Item;
  * Created by C0114105 on 2016/11/18.
  */
 public class ItemArea extends Area {
+    private Item item;
     public ItemArea(int areanum, String areaname, NextAreaInfo nextAreaInfo) {
         super(areanum, areaname, nextAreaInfo);
     }
-    public Item item;
+
+    public Item getItem() {
+        return item;
+    }
 }
