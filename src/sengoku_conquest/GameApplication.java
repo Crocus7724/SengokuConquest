4d5dd08cde794560797df54a34e6754d81f971f3
Crocus7724 <account@crocus7724.net>
2016-11-18 17:48:55 +0900

    [WIP]とりあえずScene最初の部分だけ

--- src/sengoku_conquest/GameApplication.java
+++ src/sengoku_conquest/GameApplication.java
@@ -1,22 +1,22 @@
 package sengoku_conquest;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import sengoku_conquest.character.MainCharacter;
 import sengoku_conquest.map.Area;
+import sengoku_conquest.scene.Scene;
+import sengoku_conquest.scene.StartScene;
 
 /**
  * Created by C0114105 on 2016/11/18.
  */
 public class GameApplication {
+    private List<Scene> sceneList =new ArrayList<>();
     public static final GameApplication current = new GameApplication();
     private MainCharacter mainCharacter;
 
     public void gameStart() {
-
+        nextScene(new StartScene());
     }
 
     Map<Integer, Area> map = new HashMap<Integer, Area>();
@@ -27,6 +27,19 @@ public class GameApplication {
 
     public void setMainCharacter(MainCharacter mainCharacter) {
         this.mainCharacter = mainCharacter;
+    }
+
+    public void nextScene(Scene scene){
+        if(!sceneList.isEmpty()) {
+            sceneList.get(sceneList.size() - 1).doEnd();
+        }
+        sceneList.add(scene);
+        scene.doStart();
+    }
+
+    public void previousScene(){
+        final Scene scene = sceneList.get(sceneList.size() - 2);
+        scene.doRestart();
     }
 }
 
--- src/sengoku_conquest/item/EpItem.java
+++ src/sengoku_conquest/item/EpItem.java
@@ -24,7 +24,3 @@ public class EpItem implements Item{
 
     }
 }
-
-
-
-}
