f23ad0fd616f1948a4eadb06502d8a8e7d5524e9
Crocus7724 <account@crocus7724.net>
2016-11-18 16:18:53 +0900

    [Add]Scene基本クラス作成

---
+++ src/sengoku_conquest/scene/Scene.java
@@ -0,0 +1,22 @@
+package sengoku_conquest.scene;
+
+/**
+ * Created by Yamamoto on 2016/11/18.
+ */
+public abstract class Scene {
+    public void doStart(){
+        start();
+    }
+
+    public void doEnd(){
+        end();
+    }
+
+    public void doRestart(){
+        restart();
+    }
+
+    abstract void start();
+    abstract void end();
+    abstract void restart();
+}
---
+++ src/sengoku_conquest/scene/StartScene.java
@@ -0,0 +1,28 @@
+package sengoku_conquest.scene;
+
+import sengoku_conquest.character.Job;
+import sengoku_conquest.character.JobType;
+
+/**
+ * Created by Yamamoto on 2016/11/18.
+ */
+public class StartScene extends Scene {
+
+    @Override
+    void start() {
+
+    }
+
+    @Override
+    void end() {
+
+    }
+
+    @Override
+    void restart() {
+
+    }
+
+    private void initializeJob(){
+    }
+}
