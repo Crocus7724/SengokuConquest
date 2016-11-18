d21c6200c9676cf26c3413542177d636feaaf0bc
C0114105 <c0114105@edu.teu.ac.jp>
2016-11-18 18:14:25 +0900

    コマンド移動をしてみました

--- src/sengoku_conquest/scene/AreaScene.java
+++ src/sengoku_conquest/scene/AreaScene.java
@@ -1,7 +1,124 @@
 package sengoku_conquest.scene;
 
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import sengoku_conquest.GameApplication;
+import sengoku_conquest.GameEngine;
+import sengoku_conquest.map.Area;
+import sengoku_conquest.map.EmptyArea;
+import sengoku_conquest.map.ItemArea;
+import sengoku_conquest.map.NextAreaInfo;
+
 /**
  * Created by Yamamoto on 2016/11/18.
  */
-public class AreaScene {
-}
+public class AreaScene extends Scene{
+    private Area area;
+    private GameEngine engine;
+    AreaScene(Area area){
+        this.area = area;
+        this.engine = new GameEngine();
+    }
+
+    @Override
+    void start() {
+        if(area instanceof EmptyArea){
+        waitPlayerCommand();
+        }else if (area instanceof ItemArea){
+
+        }
+    }
+
+    @Override
+    void end() {
+
+    }
+
+    @Override
+    void restart() {
+
+    }
+    private void waitPlayerCommand() {
+        engine.showMessage("コマンドを入力なさってください");
+        boolean hasitem = false;
+        engine.showMessage("1:移動");
+        engine.showMessage("2:休憩");
+        if(!GameApplication.current.getMainCharacter().getItems().isEmpty()) {
+            engine.showMessage("3:アイテム");
+            hasitem = true;
+        }
+        java.lang.String input = engine.readLineFromUserInput();
+        if(input == null){
+            engine.showMessage("コマンドを入力しなおしてください");
+            waitPlayerCommand();
+        }
+        if(input.isEmpty()|| !input.matches("[0-9]")){
+            engine.showMessage("入力が不正です");
+            waitPlayerCommand();
+        }
+        int num = Integer.parseInt(input);
+        if(num>0 &&num<(hasitem ? 4:3))     {
+            switch (num){
+                case 1:
+                    selectMoveCommand();
+            }
+        }
+
+    }
+
+    private int selectMoveCommand() {
+        Map<Integer, String> map = new HashMap<>();
+        int i = 1;
+
+            if (area.getNextAreaInfo().getEast() != -1) {
+
+                engine.showMessage(i+":東");
+                map.put(i,"E");
+                i++;
+            }
+            if (area.getNextAreaInfo().getNorth() != -1) {
+                engine.showMessage(i+":北");
+                map.put(i,"N");
+                i++;
+            }
+            if (area.getNextAreaInfo().getSouth() != -1) {
+                engine.showMessage(i+":南");
+                map.put(i,"S");
+                i++;
+            }
+            if (area.getNextAreaInfo().getWest() != -1) {
+                engine.showMessage(i+":西");
+                map.put(i,"W");
+                i++;
+            }
+        java.lang.String input = engine.readLineFromUserInput();
+        if(input == null){
+            engine.showMessage("コマンドを入力しなおしてください");
+            selectMoveCommand();
+        }
+        if(input.isEmpty()|| !input.matches("[0-9]")){
+            engine.showMessage("入力が不正です");
+            selectMoveCommand();
+        }
+            int num = Integer.parseInt(input);
+        if(num<0 &&num>=i)     {
+            String s = map.get(num);
+        if(s.equals("E")){
+            return area.getNextAreaInfo().getEast();
+        }else if(s.equals("N")){
+            return area.getNextAreaInfo().getNorth();
+        }else if(s.equals("S")){
+            return area.getNextAreaInfo().getSouth();
+        }else {
+            return area.getNextAreaInfo().getWest();
+        }
+
+        }
+        engine.showMessage("値が不正です");
+        return selectMoveCommand();
+        }
+    }
+
