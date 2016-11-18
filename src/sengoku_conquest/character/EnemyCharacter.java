2690943bc1438231c410f339cb885dd8cd0d71f3
C011457331 <c011457331@edu.teu.ac.jp>
2016-11-18 15:56:58 +0900

    job変更した

--- src/sengoku_conquest/character/EnemyCharacter.java
+++ src/sengoku_conquest/character/EnemyCharacter.java
@@ -4,7 +4,7 @@ package sengoku_conquest.character;
  * Created by C011457331 on 2016/11/18.
  */
 public class EnemyCharacter {
-    private int pops = 0;
+//    private int pops = 0;
 
     public int doSpecialAtack() {
         return 0;
