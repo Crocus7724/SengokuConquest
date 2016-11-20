e38cc180dd1b9f2bc8e99253593040a1cd120590
Crocus7724 <account@crocus7724.net>
2016-11-20 16:43:38 +0900

    [WIP]マップ初期化処理

--- src/sengoku_conquest/GameApplication.java
+++ src/sengoku_conquest/GameApplication.java
@@ -4,20 +4,25 @@ import java.util.*;
 
 import sengoku_conquest.character.MainCharacter;
 import sengoku_conquest.map.Area;
+import sengoku_conquest.map.BossArea;
+import sengoku_conquest.map.EmptyArea;
 import sengoku_conquest.scene.Scene;
 import sengoku_conquest.scene.StartScene;
+import sengoku_conquest.utilities.MappingData;
 
 /**
  * Created by C0114105 on 2016/11/18.
  */
 public class GameApplication {
-    private List<Scene> sceneList =new ArrayList<>();
+    private List<Scene> sceneList = new ArrayList<>();
 
     private int turn;
     public static final GameApplication current = new GameApplication();
     private MainCharacter mainCharacter;
 
     public void gameStart() {
+        createMapData();
+
         nextScene(new StartScene());
     }
 
@@ -31,29 +36,48 @@ public class GameApplication {
         this.mainCharacter = mainCharacter;
     }
 
-    public void nextScene(Scene scene){
-        if(!sceneList.isEmpty()) {
+    public void nextScene(Scene scene) {
+        if (!sceneList.isEmpty()) {
             sceneList.get(sceneList.size() - 1).doEnd();
         }
         sceneList.add(scene);
         scene.doStart();
     }
 
-    public void previousScene(){
+    public void previousScene() {
         final Scene scene = sceneList.get(sceneList.size() - 2);
         scene.doRestart();
     }
 
-    public void decreaseTurn(){
+    public void decreaseTurn() {
         this.turn--;
     }
 
-    public void increaseTurn(int num){
-        this.turn+=num;
+    public void increaseTurn(int num) {
+        this.turn += num;
     }
 
     public int getTurn() {
         return turn;
+    }
+
+    private void createMapData() {
+        Area[] areaList = MappingData.createArea();
+
+        Area bossArea = areaList[14 - 1];
+        map.put(14, new BossArea(bossArea.getAreaNum(), bossArea.getAreaName(), bossArea.getNextAreaInfo()));
+
+        //ボスマス周り初期化
+        for (int i = 12; i < 20; i++) {
+            if (!map.containsKey(i)) {
+                Area area = areaList[i - 1];
+                map.put(i, new EmptyArea(area.getAreaNum(), area.getAreaName(), area.getNextAreaInfo()));
+            }
+        }
+
+        //TODO : Itemマップ初期化
+        //TODO : Enemyエリア初期化
+        //TODO : 空エリア初期化
     }
 }
 
