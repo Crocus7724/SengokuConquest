2e691be6f51715d24ddedff0ff544295adb100a2
Crocus7724 <account@crocus7724.net>
2016-11-21 06:14:26 +0900

    [WIP]マップ初期化処理

--- src/sengoku_conquest/GameApplication.java
+++ src/sengoku_conquest/GameApplication.java
@@ -2,7 +2,9 @@ package sengoku_conquest;
 
 import java.util.*;
 
+import sengoku_conquest.character.EnemyCharacter;
 import sengoku_conquest.character.MainCharacter;
+import sengoku_conquest.item.Item;
 import sengoku_conquest.map.Area;
 import sengoku_conquest.map.BossArea;
 import sengoku_conquest.map.EmptyArea;
@@ -14,6 +16,7 @@ import sengoku_conquest.utilities.MappingData;
  * Created by C0114105 on 2016/11/18.
  */
 public class GameApplication {
+    private Random random=new Random();
     private List<Scene> sceneList = new ArrayList<>();
 
     private int turn;
@@ -21,6 +24,7 @@ public class GameApplication {
     private MainCharacter mainCharacter;
 
     public void gameStart() {
+
         createMapData();
 
         nextScene(new StartScene());
@@ -77,7 +81,31 @@ public class GameApplication {
 
         //TODO : Itemマップ初期化
         //TODO : Enemyエリア初期化
-        //TODO : 空エリア初期化
+
+        //空エリア初期化
+        for (Area area : areaList){
+            if(!map.containsKey(area.getAreaNum()))continue;
+
+            map.put(area.getAreaNum(),new EmptyArea(area.getAreaNum(),area.getAreaName(),area.getNextAreaInfo()));
+        }
+    }
+
+    private EnemyCharacter getEnemy(){
+        //TODO : Enemyの初期化
+        return null;
+    }
+
+    private Item getItem(){
+        //TODO : Itemの初期化
+        return null;
+    }
+
+    private int createNotContainsKey(int start,int end){
+        final int i = random.nextInt(end - start) + start;
+
+        if(!map.containsKey(i))return i;
+
+        return createNotContainsKey(start,end);
     }
 }
 
