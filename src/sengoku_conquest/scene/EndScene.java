4f642fc96cc41ed29aeb965d1342a7ed48e58924
C0114544 <c01145447a@edu.teu.ac.jp>
2016-11-22 17:21:23 +0900

    EndScene取り敢えず手直ししたYO(2016.11,22.17:21)

--- src/sengoku_conquest/scene/EndScene.java
+++ src/sengoku_conquest/scene/EndScene.java
@@ -19,6 +19,7 @@ public class  EndScene extends Scene{
         engine.showMessage("GAME CLEAR!!/n" +
                 "THANK YOU FOR PLAYING!!/n" +
                 "見事全国を制覇しました!!/n" +
+                "           STAFF/n" +
                 "チームリーダー　　　江元和樹/n" +
                 "設計担当者　　　　　伊賀成寿/n" +
                 "プログラミング担当　山本一樹/n" +
@@ -28,6 +29,32 @@ public class  EndScene extends Scene{
 
     @Override
     void end() {
+        if(getTurn() >= 0){
+            engine.showMessage("GAME OVER.../n" +
+                    "THANK YOU FOR PLAYING.../n" +
+                    "行動ターンがなくなってしまった！！/n" +
+                    "           STAFF/n" +
+                    "チームリーダー　　　江元和樹/n" +
+                    "設計担当者　　　　　伊賀成寿/n" +
+                    "プログラミング担当　山本一樹/n" +
+                    "マーケテイング担当　山田諒　/n" +
+                    "プレイ担当　　　　　YOU　　 /n");
+        }
+        if(getMainCharacter().getStatus().getCurrentHp >= 0){
+            engine.showMessage("GAME OVER.../n" +
+                    "THANK YOU FOR PLAYING.../n" +
+                    "戦闘に敗北してしまった...世界は暗黒に包まれた.../n" +
+                    "           STAFF/n" +
+                    "チームリーダー　　　江元和樹/n" +
+                    "設計担当者　　　　　伊賀成寿/n" +
+                    "プログラミング担当　山本一樹/n" +
+                    "マーケテイング担当　山田諒　/n" +
+                    "プレイ担当　　　　　YOU　　 /n");
+        }
+
+        else{
+            engine.showMessage("どこかでバグが起きてるYO/n");
+        }
 
     }
 
