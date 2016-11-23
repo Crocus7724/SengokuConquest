7e112777303d8d25f5dcd03b50682958d8493553
C0114105 <c0114105@edu.teu.ac.jp>
2016-11-24 00:39:37 +0900

    AreaSeceneの休憩コマンドの実装などです

--- src/sengoku_conquest/map/BossArea.java
+++ src/sengoku_conquest/map/BossArea.java
@@ -1,10 +1,18 @@
 package sengoku_conquest.map;
 
+import sengoku_conquest.character.Character;
+import sengoku_conquest.character.EnemyCharacter;
+
 /**
  * Created by C0114105 on 2016/11/18.
  */
 public class BossArea extends Area {
     public BossArea(int areanum, String areaname, NextAreaInfo nextAreaInfo) {
         super(areanum, areaname, nextAreaInfo);
+    }
+
+
+
+
     }
 }
