462d7ca623a8503dc56fe1de8692acca7bc1f212
Crocus7724 <account@crocus7724.net>
2016-11-25 00:38:18 +0900

    [Add]改行しない出力を追加

--- src/sengoku_conquest/GameEngine.java
+++ src/sengoku_conquest/GameEngine.java
@@ -20,6 +20,10 @@ public final class GameEngine {
         this.indentation=indentation;
     }
 
+    public void show(String message){
+        System.out.print(getIndentation()+message);
+    }
+
     public void showMessage(String message) {
         System.out.println(getIndentation()+message);
     }
