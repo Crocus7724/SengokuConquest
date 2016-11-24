4a2e66530d12b0cadc8c56f3d2e99e13691201a8
Crocus7724 <account@crocus7724.net>
2016-11-24 16:03:04 +0900

    [Add]BattleSceneの基本的部分作成

--- src/sengoku_conquest/scene/BattleScene.java
+++ src/sengoku_conquest/scene/BattleScene.java
@@ -1,7 +1,30 @@
 package sengoku_conquest.scene;
 
+import sengoku_conquest.character.Character;
+import sengoku_conquest.character.EnemyCharacter;
+
 /**
  * Created by Yamamoto on 2016/11/18.
  */
-public class BattleScene {
+public class BattleScene extends Scene {
+    private final EnemyCharacter enemy;
+
+    public BattleScene(EnemyCharacter enemy){
+        this.enemy=enemy;
+    }
+
+    @Override
+    void start() {
+        
+    }
+
+    @Override
+    void end() {
+
+    }
+
+    @Override
+    void restart() {
+
+    }
 }
