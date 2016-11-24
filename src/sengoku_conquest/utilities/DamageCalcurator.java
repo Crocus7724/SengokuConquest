98cbc009f9beaa7866a6491e48e1fbd7201d039d
Crocus7724 <account@crocus7724.net>
2016-11-25 01:06:26 +0900

    [Add]ダメージ計算用のクラス作成

---
+++ src/sengoku_conquest/utilities/DamageCalcurator.java
@@ -0,0 +1,16 @@
+package sengoku_conquest.utilities;
+
+import java.util.Random;
+
+/**
+ * Created by Yamamoto on 2016/11/25.
+ */
+public class DamageCalcurator {
+    private static Random r=new Random();
+
+    public static int calc(int attack,int def){
+        final int n=r.nextInt(3)-1;
+
+        return (attack-def+n);
+    }
+}
