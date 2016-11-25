83d567cc935e55abb9106db8cf773bd059868f52
C0114544 <c01145447a@edu.teu.ac.jp>
2016-11-25 13:59:22 +0900

    EndScene取り敢えず手直しの手直ししたYO(2016.11,25.11:40)

--- src/sengoku_conquest/scene/EndScene.java
+++ src/sengoku_conquest/scene/EndScene.java
@@ -17,20 +17,7 @@ public class  EndScene extends Scene{
 
     @Override
     void start() {
-        engine.showMessage("GAME CLEAR!!/n" +
-                "THANK YOU FOR PLAYING!!/n" +
-                "見事全国を制覇しました!!/n" +
-                "           STAFF/n" +
-                "チームリーダー　　　江元和樹/n" +
-                "設計担当者　　　　　伊賀成寿/n" +
-                "プログラミング担当　山本一樹/n" +
-                "マーケテイング担当　山田諒　/n" +
-                "プレイ担当　　　　　YOU　　 /n");
-    }
-
-    @Override
-    void end() {
-        if(GameApplication.current.getTurn() >= 0){
+        if(GameApplication.current.getTurn() <= 0){
             engine.showMessage("GAME OVER.../n" +
                     "THANK YOU FOR PLAYING.../n" +
                     "行動ターンがなくなってしまった！！/n" +
@@ -41,7 +28,7 @@ public class  EndScene extends Scene{
                     "マーケテイング担当　山田諒　/n" +
                     "プレイ担当　　　　　YOU　　 /n");
         }
-        if(GameApplication.current.getMainCharacter().getStatus().getCurrentHp >= 0){
+        if(GameApplication.current.getMainCharacter().getStatus().getCurrentHp() <= 0){
             engine.showMessage("GAME OVER.../n" +
                     "THANK YOU FOR PLAYING.../n" +
                     "戦闘に敗北してしまった...世界は暗黒に包まれた.../n" +
@@ -57,6 +44,26 @@ public class  EndScene extends Scene{
             engine.showMessage("どこかでバグが起きてるYO/n");
         }
 
+
+
+
+
+        engine.showMessage("GAME CLEAR!!/n" +
+                "THANK YOU FOR PLAYING!!/n" +
+                "見事全国を制覇しました!!/n" +
+                "           STAFF/n" +
+                "チームリーダー　　　江元和樹/n" +
+                "設計担当者　　　　　伊賀成寿/n" +
+                "プログラミング担当　山本一樹/n" +
+                "マーケテイング担当　山田諒　/n" +
+                "プレイ担当　　　　　YOU　　 /n");
+
+        System.exit(1);
+    }
+
+    @Override
+    void end() {
+     
     }
 
     @Override
