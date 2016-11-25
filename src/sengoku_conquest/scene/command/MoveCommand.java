8c20d1480f4e14fc7c8e5910a5b804adad01bfb3
C011457331 <c011457331@edu.teu.ac.jp>
2016-11-25 15:25:13 +0900

    MainChara,StartScene変更

---
+++ src/sengoku_conquest/scene/command/MoveCommand.java
@@ -0,0 +1,13 @@
+package sengoku_conquest.scene.command;
+
+import sengoku_conquest.GameEngine;
+
+/**
+ * Created by C011457331 on 2016/11/25.
+ */
+public class MoveCommand extends AreaCommandHandler{
+    private GameEngine engine=GameEngine.current;
+
+    GameEngine gm = new GameEngine();
+    gm.showMessage("");
+}
