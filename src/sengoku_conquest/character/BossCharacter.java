0022116f95fdd3f783f23a07a8d99b2347c69450
C0114105 <c0114105@edu.teu.ac.jp>
2016-11-25 14:48:49 +0900

    BossChara実装だけどこのクラスでボスのＨＰ半減したらフラグ反転するメソッド作ってもよいかもしれないと思ったてか作らなきゃダメだったらごめんなさい

---
+++ src/sengoku_conquest/character/BossCharacter.java
@@ -0,0 +1,19 @@
+package sengoku_conquest.character;
+
+/**
+ * Created by C0114105 on 2016/11/25.
+ */
+public class BossCharacter extends EnemyCharacter {
+    public boolean isCharged;//ためたのかどうか
+    public BossCharacter(){
+        super("織田信長",4);
+        this.isCharged = false;
+
+    }
+
+    public boolean getisCharged(){return isCharged;}
+    //↑一応作ったけどいるかどうか不明メソッド名も仮
+
+
+
+}
